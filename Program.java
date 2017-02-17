2d2cd89f7724bc1c1ada7813a648e1257f2758cc
knvssandeep855 <knvssandeep@outlook.com>
2017-02-16 19:55:24 -0600

    added new function strLen

--- Program.java
+++ Program.java
@@ -4,7 +4,16 @@ package com.Sandeep_Udemy;
  * Created by knvss on 2/16/2017.
  */
 public class Program {
+
+    public static int strLen(String str){
+        int len;
+        len = str.length();
+        return len;
+    }
+
     public static void main(String[] args){
-        System.out.println("Hello World!!");
+        String txt = "Sandeep";
+        System.out.println(txt);
+        System.out.println("The length of '"+ txt +"' is : "+ strLen(txt));
     }
 }
